b19dfa244a9776a1f46b7f28c76ffbed3d49b65e
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-09 13:27:07 +0100

    added the missing () after the  execute

--- src/Classes/Model/ToDoModel.php
+++ src/Classes/Model/ToDoModel.php
@@ -12,8 +12,12 @@ class ToDoModel {
     }
 
     public function getTasks(){
-        $query = $this->db->prepare("SELECT `task` FROM `task_list` WHERE `completed` = '0';");
-        $query->execute;
-        return $query->fetchAll(\PDO::FETCH_ASSOC);
+        $query = $this->db->prepare("SELECT `task` FROM `task_list` WHERE `completed` = 0;");
+        $query->execute();
+        return $query->fetchAll();
+    }
+
+    public function addTasks(){
+        $query = $this->db->prepare("INSERT INTO `to_do_list` (`task`) VALUES (:newTask); ");
     }
 }
