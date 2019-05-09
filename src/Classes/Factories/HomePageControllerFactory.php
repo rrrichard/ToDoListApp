0da25923bf1f9f41263b3079491eb2721c8d6e46
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-09 13:26:28 +0100

    added ToDoModel in the invoke function and in the return

--- src/Classes/Factories/HomePageControllerFactory.php
+++ src/Classes/Factories/HomePageControllerFactory.php
@@ -10,6 +10,7 @@ class HomePageControllerFactory {
     public function __invoke(ContainerInterface $container)
     {
         $renderer = $container->get('renderer');
-        return new HomePageController($renderer);
+        $toDoModel = $container->get('ToDoModel');
+        return new HomePageController($renderer, $toDoModel);
     }
 }
