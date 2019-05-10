735a2da7e6eaaf2da0bb4411808cc8124a28f696
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-10 16:03:41 +0100

    removed the route to the date add

--- src/routes.php
+++ src/routes.php
@@ -10,19 +10,5 @@ return function (App $app) {
     $app->get('/', 'HomePageController');
     $app->post('/add', 'AddToDoController');
     $app->post('/completed','CompletedTask');
-    $app->post('/date', 'Date');
 
-//    $app->post('/', function ($request, $response, $args){
-//        $newTask = $request->getParam('add_form');
-//    });
-
-
-
-//    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-//        // Sample log message
-//        $container->get('logger')->info("Slim-Skeleton '/' route");
-//
-//        // Render index view
-//        return $container->get('renderer')->render($response, 'index.phtml', $args);
-//    });
 };
