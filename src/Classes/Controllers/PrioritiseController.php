7267f6c715b17abd60341a43af3040e1eaa7e70d
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-14 21:59:25 +0100

    to sort out how important task is

--- src/Classes/Controllers/PrioritiseController.php
+++ src/Classes/Controllers/PrioritiseController.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace ToDo\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use ToDo\Model\ToDoModel;
+
+class PrioritiseController {
+
+    private $toDoModel;
+
+    public function __construct(ToDoModel $toDoModel)
+    {
+        $this->toDoModel = $toDoModel;
+    }
+
+    public function __invoke(Request $request, Response $response, $args)
+    {
+        $selectedPriority = $request->getParsedBody();
+        $hiddenId = $selectedPriority['hiddenId'];
+        $priorityId = $selectedPriority['priority'];
+        $this->toDoModel->prioritiseTasks($priorityId, $hiddenId);
+        return $response->withRedirect('/');
+    }
+}
